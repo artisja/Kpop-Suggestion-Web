d90313b21b241762865c24faf3676790d5ce1805
Justin Artis <jusart1s8@gmail.com>
2020-02-16 18:00:30 -0500

    initial commit

---
+++ src/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HeaderComponent.css';
+
+class HeaderComponent extends React.Component{
+  render() {
+    return(
+<div className="header-div">
+<div>
+  <h2 class="ui icon center aligned header">
+    <i aria-hidden="true" class="users circular icon"></i>
+    <div class="content">Friends</div>
+  </h2>
+  <div class="ui horizontal divider">Or</div>
+</div>
+</div>
+)
+  }
+}
+export default HeaderComponent;
---
+++ src/route.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+
+
+class AppRouter extends React.Component {
+  constructor() {
+
+  }
+}
